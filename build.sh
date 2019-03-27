9dc859592554e025ed55f8eeb5e69e600e0b5c39
Patrick Spek <p.spek@tyil.nl>
2019-03-27 08:12:37 +0100

    Add a simple script to make building simpler

---
+++ build.sh
@@ -0,0 +1,53 @@
+#! /usr/bin/env sh
+
+readonly BASEDIR=$(CDPATH="" cd -- "$(dirname -- "$0")" && pwd -P)
+
+main()
+{
+	DATE=$(date +%F)
+
+	if [ $# -gt 0 ]
+	then
+		for base in "$@"
+		do
+			DATE=$DATE BASE="$base" build_docker
+		done
+
+		# Intended lack of cleanup, as this step is generally a manual build. As
+		# this is often used for debugging, cleanup is often not wanted. Manual
+		# cleanup is still possible, though.
+
+		return 0
+	fi
+
+	printf "%s\n" "Building all known images!"
+
+	for base in "$BASEDIR/docker/"*
+	do
+		DATE=$DATE BASE="$(basename $base)" build_docker
+	done
+
+	cleanup
+}
+
+build_docker()
+{
+	[ -z "$DATE" ] && DATE=$(date +%F)
+	[ -z "$BASE" ] && BASE=debian
+	[ -z "$TAG" ] && TAG=$BASE
+
+	make BASE="${BASE}" TAG="${TAG:-$BASE}-$DATE" docker \
+		&& docker push "tyil/perl6:$BASE-$DATE" \
+		&& docker tag \
+			"$(docker images | awk '/^tyil\/perl6\s+'"$BASE"'-'"$DATE"'\s+/ { print $3 }')" \
+			tyil/perl6:debian-latest \
+		&& docker push "tyil/perl6:$BASE-latest"
+}
+
+cleanup()
+{
+	docker rm $(docker ps -a -q)
+	docker rmi $(docker images | grep '^<none>' | awk '{ print $3 }')
+}
+
+main "$@"
